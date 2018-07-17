49f9e75351e18fbd8f21c698f262c10f5de5400e
Aylin DeBruyne <adebruyne88@gmail.com>
2018-07-17 13:59:29 -0400

    create initial repo

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const App = () => {
+  // React.createElement('h1',null, 'hello');
+  return (
+    // JSX
+    <div className = "container">
+  <h1>loe</h1>
+    <button>+</button>
+    <div className="counter-box">
+        <div className= "counter">3</div>
+        <div className= "counter">1</div>
+        <div className= "counter">1</div>
+    </div>
+
+  </div>
+
+
+
+)
+};
+
+
+export default App;
