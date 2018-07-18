98f327e21b758f79787ce300183d40c0dbcf9355
Aylin DeBruyne <adebruyne88@gmail.com>
2018-07-18 10:08:55 -0400

    function to start counter

--- src/Counter.js
+++ src/Counter.js
@@ -2,12 +2,49 @@ import React from 'react';
 
 //Rule #1: extend React.Component
 class Counter extends React.Component {
+    //Rule #4: accept props in the constructor method
+   
+    //////////////////////////SETUP/////////////////////
+    constructor(props){
+        super(props);
+        // The props being the instructions being passed to the line cook
+        //the state is the "line cook's stuff"
+        //it is always an object
+        this.state = {
+            currentValue: props.initialValue
+        };
+
+        setInterval(this._increaseValue, 1000);
+    }
+
+
+
+
+    //Always write helper functions as arrow functions. Helps keep the bugs away
+    _increaseValue = () => {
+        //calculate the new currentValue
+        let newCurrentValue = this.state.currentValue + 1;
+
+        //then set the new currentValue in state
+        //but I must call this.setState I cannot alter this.state directly
+
+
+        this.setState({
+            currentValue: newCurrentValue
+        });
+    }
+
+
+
+
+
+    //////////////////////RENDER////////////////////////
     //Rule #2: must have render method
     render(){
         //Rule #3: must return some JSX or a call to React.CreateELement
         return (
             <div className = 'counter'>
-            3
+            {this.state.currentValue}
             </div>
         );
     }
